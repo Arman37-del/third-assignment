7c69a9f3e861113d3b0a127fd480956591937bf7
ausiaam03 <77216397+ausiaam03@users.noreply.github.com>
2021-02-01 21:09:32 +0600

    script

---
+++ assignment.js
@@ -0,0 +1,45 @@
+// 
+
+
+
+function kilometerToMeter(kilometer) {
+    var meter = kilometer / 1000;
+    return meter;
+}
+
+function budgetCalculator(purchase) {
+    var budget = 0;
+    if (purchase <= 10) {
+        budget = purchase * 50;
+    } else if (purchase <= 20) {
+        var clock = 10 * 50;
+        var remaining = purchase - 10;
+        var phone = remaining * 100;
+        budget = clock + phone;
+    } else {
+        var clock = 10 * 50;
+        var phone = 10 * 100;
+        var remaining = purchase - 20;
+        var laptop = remaining * 500;
+        budget = clock + phone + laptop;
+    }
+    return budget;
+}
+
+function hotelCost(firstMonth, secondMonth) {
+    var total = firstMonth + secondMonth;
+    return total;
+}
+
+
+function megaFriend(name) {
+    var name = ['siam', 'sefat', 'shourov'];
+    var max = name[0];
+    for (var i = 0; i < name.length; i++) {
+        var element = name[i];
+        if (element > max) {
+            max = element;
+        }
+    }
+    return max;
+}
